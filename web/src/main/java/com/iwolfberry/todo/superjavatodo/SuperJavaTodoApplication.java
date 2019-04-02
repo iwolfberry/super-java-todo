b79a75f41fb219c0be5f9be65bbe1b0856d45d82
gengjiarong@gmail.com <gengjiarong@gmail.com>
2019-04-02 22:41:31 +0800

    sprint boot 模块化拆分

---
+++ web/src/main/java/com/iwolfberry/todo/superjavatodo/SuperJavaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.iwolfberry.todo.superjavatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperJavaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperJavaTodoApplication.class, args);
+	}
+
+}
---
+++ web/test/java/com/iwolfberry/todo/superjavatodo/SuperJavaTodoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iwolfberry.todo.superjavatodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperJavaTodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
